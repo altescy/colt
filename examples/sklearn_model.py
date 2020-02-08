8c04a21dada278cfea7aa58b4ec32ae1f8a8ebb1
altescy <altescy@fastmail.com>
2020-02-08 13:00:08 +0900

    update sklearn example

--- examples/sklearn_model.py
+++ examples/sklearn_model.py
@@ -1,28 +1,41 @@
+import typing as tp
 import colt
+import importlib
 
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.base import BaseEstimator
 
 
-colt.register("voting-classifier")(VotingClassifier)
-colt.register("rfc")(RandomForestClassifier)
-colt.register("svc")(SVC)
+@colt.register("sklearn-model", constructor="from_dict")
+class SklearnModelWrapper:
+    @classmethod
+    def from_dict(cls, model_dict: tp.Dict[str, tp.Any]) -> BaseEstimator:
+        model_path = model_dict.pop("@model")
+        model_path = "sklearn." + model_path
+
+        module_path, model_name = model_path.rsplit(".", 1)
+
+        module = importlib.import_module(module_path)
+        model_cls = getattr(module, model_name)
+
+        if not issubclass(model_cls, BaseEstimator):
+            raise ValueError(f"{model_path} is not an estimator")
+
+        return model_cls(**model_dict)
 
 
 if __name__ == "__main__":
     config = {
-        "@type": "voting-classifier",
+        "@type": "sklearn-model",
+        "@model": "ensemble.VotingClassifier",
         "estimators": [
-            ("rfc", {"@type": "rfc", "n_estimators": 10}),
-            ("svc", {"@type": "svc", "gamma": "scale"}),
+            ("rfc", {"@type": "sklearn-model", "@model": "ensemble.RandomForestClassifier", "n_estimators": 10}),
+            ("svc", {"@type": "sklearn-model", "@model": "svm.SVC", "gamma": "scale"}),
         ]
     }
 
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    X, y = load_iris(return_X_y=True)
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
     model = colt.build(config)
@@ -30,4 +43,3 @@ if __name__ == "__main__":
 
     valid_accuracy = model.score(X_valid, y_valid)
     print(f"valid_accuracy: {valid_accuracy}")
-
