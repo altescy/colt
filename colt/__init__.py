114de7ecc21b73fd7e38bf06a791787902ce8443
altescy <altescy@fastmail.com>
2020-05-03 19:10:21 +0900

    yapf

--- colt/__init__.py
+++ colt/__init__.py
@@ -8,7 +8,9 @@ def register(name: str, constructor: str = None):
     def decorator(T: tp.Type):
         TypeStore.add(name, T, constructor)
         return T
+
     return decorator
+
 
 def build(config: tp.Any, typekey: str = None) -> tp.Any:
     builder = ColtBuilder(typekey)
