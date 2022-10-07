5e197c2c3ce89ba67689d5864f482c63b8100c20
altescy <altescy@fastmail.com>
2022-10-08 01:09:58 +0900

    modify lazy args

--- colt/lazy.py
+++ colt/lazy.py
@@ -10,15 +10,15 @@ T = TypeVar("T")
 class Lazy(Generic[T]):
     def __init__(
         self,
-        builder: "ColtBuilder",
         config: Any,
         cls: Optional[Type[T]] = None,
-        param_name: str = "",
+        param_name: Optional[str] = None,
+        builder: Optional["ColtBuilder"] = None,
     ) -> None:
-        self._builder = builder
         self._cls = cls
         self._config = config or {}
-        self._param_name = param_name
+        self._param_name = param_name or ""
+        self._builder = builder or ColtBuilder()
 
     def construct(self, **kwargs: Any) -> T:
         config = {**self._config, **kwargs}
