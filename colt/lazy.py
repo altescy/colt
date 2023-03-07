25656aa79385c6bf8a34332291f91de1ddc36786
altescy <altescy@fastmail.com>
2023-03-07 15:33:18 +0900

    make lazy param traceable

--- colt/lazy.py
+++ colt/lazy.py
@@ -11,6 +11,7 @@ class Lazy(Generic[T]):
     def __init__(
         self,
         config: Any,
+        param_name: str = "",
         cls: Optional[Type[T]] = None,
         builder: Optional["ColtBuilder"] = None,
     ) -> None:
@@ -18,8 +19,9 @@ class Lazy(Generic[T]):
 
         self._cls = cls
         self._config = config or {}
+        self._param_name = param_name
         self._builder = builder or ColtBuilder()
 
     def construct(self, **kwargs: Any) -> T:
         config = {**self._config, **kwargs}
-        return self._builder(config, self._cls)
+        return self._builder._build(config, self._param_name, self._cls)
