dbe04e9dad2bbd0d9129bed8af2120effea59477
altescy <altescy@fastmail.com>
2024-09-19 23:09:51 +0900

    support nested-field update for lazy construction

--- colt/lazy.py
+++ colt/lazy.py
@@ -1,15 +1,6 @@
 import typing
-from typing import (
-    Any,
-    Callable,
-    Generic,
-    Mapping,
-    Optional,
-    Sequence,
-    Type,
-    TypeVar,
-    Union,
-)
+from copy import deepcopy
+from typing import Any, Generic, Mapping, Optional, Sequence, Type, TypeVar, Union
 
 from colt.utils import update_field
 
@@ -56,6 +47,18 @@ class Lazy(Generic[T]):
             update_field(self._config, k, v)
         self._builder.dry_run(self._config, self._cls, param_name=self._param_name)
 
-    def construct(self, **kwargs: Any) -> T:
-        config = {**self._config, **kwargs}
+    def construct(
+        self,
+        *args: Mapping[Union[int, str, Sequence[Union[int, str]]], Any],
+        **kwargs: Any,
+    ) -> T:
+        if args or kwargs:
+            config = deepcopy(self._config)
+            for arg in args:
+                for field, value in arg.items():
+                    update_field(config, field, value)
+            for k, v in kwargs.items():
+                update_field(config, k, v)
+        else:
+            config = self._config
         return self._builder._build(config, self._param_name, self._cls)
