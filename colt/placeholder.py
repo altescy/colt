fcddf1a736336edb67a987215808f2bad1211df5
altescy <altescy@fastmail.com>
2024-09-20 16:21:25 +0900

    use generic

--- colt/placeholder.py
+++ colt/placeholder.py
@@ -1,14 +1,16 @@
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 from colt.utils import issubtype
 
+T = TypeVar("T")
 
-class Placeholder:
-    def __init__(self, annotation: Any) -> None:
+
+class Placeholder(Generic[T]):
+    def __init__(self, annotation: T) -> None:
         self._annotation = annotation
 
     @property
-    def type_hint(self) -> Any:
+    def type_hint(self) -> T:
         return self._annotation
 
     def match_type_hint(self, annotation: Any) -> bool:
